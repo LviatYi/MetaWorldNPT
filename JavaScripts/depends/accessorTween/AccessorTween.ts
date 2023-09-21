72e47dfb2bb27ecc5061fd4833733604974e7d77
lviatyi <jiajun.yi@appshahe.com>
2023-09-21 19:19:28 +0800

    feat: update AccessorTween1.2.0b

--- JavaScripts/depends/accessorTween/AccessorTween.ts
+++ JavaScripts/depends/accessorTween/AccessorTween.ts
@@ -461,7 +461,7 @@ export class SingleTweenTask {
  * @author LviatYi
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
- * @version 1.1.5b
+ * @version 1.2.0b
  */
 class AccessorTween implements IAccessorTween {
     private _tasks: TweenTask<unknown>[] = [];
