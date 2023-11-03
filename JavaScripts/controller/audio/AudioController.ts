570c7cdd5738c1a6a9af4e652198a81d45d021bd
lviatyi <jiajun.yi@appshahe.com>
2023-11-03 15:52:56 +0800

    feat: update AC

--- JavaScripts/controller/audio/AudioController.ts
+++ JavaScripts/controller/audio/AudioController.ts
@@ -1,7 +1,7 @@
-import { Singleton } from "../depends/singleton/Singleton";
-import { GameConfig } from "../config/GameConfig";
-import { ISoundElement } from "../config/Sound";
-import GToolkit from "../util/GToolkit";
+import { Singleton } from "../../depends/singleton/Singleton";
+import { GameConfig } from "../../config/GameConfig";
+import { ISoundElement } from "../../config/Sound";
+import GToolkit from "../../util/GToolkit";
 import SoundService = mw.SoundService;
 
 export enum SoundIDEnum {
@@ -36,7 +36,7 @@ export enum SoundIDEnum {
  * @author LviatYi
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
- * @version 1.0.0a
+ * @version 1.1.0a
  */
 export default class AudioController extends Singleton<AudioController>() {
 //region Member
