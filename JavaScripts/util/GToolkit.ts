d84da163061477e863dfac9f66e1864e75465030
lviatyi <jiajun.yi@appshahe.com>
2023-11-09 11:20:15 +0800

    feat: update GToolkit0.5.3

--- JavaScripts/util/GToolkit.ts
+++ JavaScripts/util/GToolkit.ts
@@ -90,7 +90,7 @@ export enum GenderTypes {
  * @author LviatYi
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
- * @version 0.5.1a
+ * @version 0.5.3a
  * @alpha
  */
 class GToolkit {
