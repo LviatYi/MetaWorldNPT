2ef3e91b4ce67e73cda1b3d56f6b7617cc507d03
lviatyi <jiajun.yi@appshahe.com>
2023-11-21 14:35:28 +0800

    feat: add hack.d.ts

---
+++ JavaScripts/declaration/mw/hack.d.ts
@@ -0,0 +1,9 @@
+declare namespace mw {
+    /**
+     * @ignore
+     * @description 用于标记ui类型
+     * @effect  调用生效
+     * @param InType
+     */
+    function tryGenerateTsWidgetTypeByUEObject(InType: UE.Widget): mw.Widget;
+}
