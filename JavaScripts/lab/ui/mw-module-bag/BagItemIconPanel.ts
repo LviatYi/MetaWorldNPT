0db0f6fed70779ac5c292578b784f20e3f5f4a50
lviatyi <jiajun.yi@appshahe.com>
2023-11-26 16:14:24 +0800

    test: add ScrollView test

---
+++ JavaScripts/lab/ui/mw-module-bag/BagItemIconPanel.ts
@@ -0,0 +1,93 @@
+import BagItemIcon_Generate from "../../../ui-generate/mw-module-bag/BagItemIcon_generate";
+import IUnique from "../../../depends/yoact/IUnique";
+import IScrollViewItem from "../../../depends/scroll-view/IScrollViewItem";
+import { Yoact } from "../../../depends/yoact/Yoact";
+import GToolkit from "../../../util/GToolkit";
+import bindYoact = Yoact.bindYoact;
+
+export class BagItem implements IUnique {
+    public id: number;
+    public name: string;
+    public desc: string;
+    public count: number;
+
+    constructor(id: number,
+                name: string,
+                desc: string,
+                count: number) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.count = count;
+    }
+
+    public move(updated: this): boolean {
+        this.id = updated.id;
+        this.name = updated.name;
+        this.desc = updated.desc;
+        this.count = updated.count
+        ;
+        return false;
+    }
+
+    public primaryKey(): number {
+        return this.id;
+    }
+}
+
+export default class BagItemIcon extends BagItemIcon_Generate implements IScrollViewItem<BagItem> {
+//#region Member
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld UI Event
+
+    protected onAwake(): void {
+        super.onAwake();
+        this.canUpdate = true;
+
+//#region Member init
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Widget bind
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Event subscribe
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onUpdate() {
+    }
+
+    protected onShow() {
+    }
+
+    protected onHide() {
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region UI Behavior
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region IScrollViewItem
+
+    public bindData(data: BagItem): void {
+        bindYoact(() => this.mItemNum.text = data.count.toString());
+    }
+
+    public get clickObj(): mw.StaleButton {
+        return this.mItemBtn;
+    }
+
+    public onSetSelect(bool: boolean): void {
+        GToolkit.trySetVisibility(this.mImgSelect, bool);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Event Callback
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
---
+++ JavaScripts/lab/ui/mw-module-bag/BagPanel.ts
@@ -0,0 +1,84 @@
+import BagMain_Generate from "../../../ui-generate/mw-module-bag/BagMain_generate";
+import ScrollView from "../../../depends/scroll-view/ScrollView";
+import BagItemIcon, { BagItem } from "./BagItemIconPanel";
+import YoactArray from "../../../depends/yoact/YoactArray";
+import IYoactArray from "../../../depends/yoact/IYoactArray";
+import GToolkit from "../../../util/GToolkit";
+
+export default class BagPanel extends BagMain_Generate {
+//#region Member
+    private _scrollView: ScrollView<BagItem, BagItemIcon>;
+
+    private _outData: IYoactArray<BagItem>;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region MetaWorld UI Event
+
+    protected onAwake(): void {
+        super.onAwake();
+        this.canUpdate = true;
+
+//#region Member init
+        this._outData = new YoactArray();
+        this._outData.setAll(
+            [
+                new BagItem(1, "item-1", "desc-1", 9),
+                new BagItem(2, "item-2", "desc-2", 99),
+                new BagItem(3, "item-3", "desc-3", 999),
+                new BagItem(4, "item-4", "desc-4", 9999),
+            ],
+        );
+        this._scrollView = new ScrollView<BagItem, BagItemIcon>(
+            this._outData,
+            BagItemIcon,
+            this.mScrollBox,
+            this.mContent,
+        ).listenOnItemSelect((key) => {
+            if (key === null) {
+                GToolkit.trySetVisibility(this.infoCanvas, false);
+            }
+            const data = this._outData.getItem(key);
+            this.mName.text = data.name;
+            this.mDesc.text = data.desc;
+            this.mNum.text = `数量${data.count}`;
+        });
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Widget bind
+//#endregion ------------------------------------------------------------------------------------------
+
+//#region Event subscribe
+//#endregion ------------------------------------------------------------------------------------------
+    }
+
+    protected onUpdate() {
+    }
+
+    protected onShow() {
+    }
+
+    protected onHide() {
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Init
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region UI Behavior
+
+    private _count: number = 4;
+
+    public addItem() {
+        this._outData.addItem(new BagItem(++this._count, `item-${this._count}`, `desc-${this._count}`, 999));
+    }
+
+    public removeItem() {
+        this._outData.removeItem(this._count--);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Event Callback
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
