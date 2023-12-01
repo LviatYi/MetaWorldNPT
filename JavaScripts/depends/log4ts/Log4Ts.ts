dc41dc762c2907b2db11c341f4e30695fb68f16f
lviatyi <jiajun.yi@appshahe.com>
2023-12-01 11:14:22 +0800

    feat: add Log4Ts1.0.0b

---
+++ JavaScripts/depends/log4ts/Log4Ts.ts
@@ -0,0 +1,265 @@
+/**
+ * 日志等级.
+ */
+export enum DebugLevels {
+    /**
+     * 无日志.
+     */
+    Silent = 0,
+    /**
+     * 信息. 包含 warn error.
+     */
+    Info,
+    /**
+     * 开发. 包含 log warn error.
+     */
+    Dev,
+}
+
+/**
+ * 日志 lambda.
+ */
+export type logString = (...params: unknown[]) => string;
+
+export type Announcer = { name: string };
+
+export class Log4TsConfig {
+    /**
+     * 日志等级.
+     */
+    public debugLevel: DebugLevels = DebugLevels.Dev;
+
+    /**
+     * 白名单.
+     * @desc only announcers in _whiteList could be printed.
+     */
+    private _whiteList: Set<string> = new Set<string>();
+
+    /**
+     * 黑名单.
+     * @desc announcers in _blackList will never be printed.
+     */
+    private _blackList: Set<string> = new Set<string>();
+
+    private _filter: (name: string) => boolean = null;
+
+    /**
+     * 设定过滤器.
+     * @desc 当过滤器存在时 仅过滤器生效.
+     * @desc 以支持更多的客制化过滤选项.
+     * @param filter
+     */
+    public setFilter(filter: (name: string) => boolean): this {
+        this._filter = filter;
+        return this;
+    }
+
+    /**
+     * 重置过滤器.
+     * @desc 重置后将使用白名单与黑名单进行过滤.
+     */
+    public resetFilter(): this {
+        this._filter = null;
+        return this;
+    }
+
+    /**
+     * 添加 白名单.
+     * @param names
+     */
+    public addWhiteList(...names: (string | string[])[]): this {
+        names.forEach(name => {
+            if (Array.isArray(name)) {
+                name.forEach(n => this._whiteList.add(n));
+            } else {
+                this._whiteList.add(name);
+            }
+        });
+        return this;
+    }
+
+    /**
+     * 添加 黑名单.
+     * @param names
+     */
+    public addBlackList(...names: (string | string[])[]): this {
+        names.forEach(name => {
+            if (Array.isArray(name)) {
+                name.forEach(n => this._blackList.add(n));
+            } else {
+                this._blackList.add(name);
+            }
+        });
+        return this;
+    }
+
+    private inWhiteList(announcer: Announcer): boolean {
+        return this._whiteList.size > 0 && this._whiteList.has(announcer.name);
+    }
+
+    private inBlackList(announcer: Announcer): boolean {
+        return this._blackList.has(announcer.name);
+    }
+
+    /**
+     * 检查 Announcer 是否可打印.
+     * @param announcer
+     */
+    public checkAnnouncer(announcer: Announcer): boolean {
+        return this._filter ?
+            this._filter(announcer.name) :
+            this.inWhiteList(announcer) && !this.inBlackList(announcer);
+    }
+
+//#region Shorter Builder
+    /**
+     * short for {@link setFilter}.
+     * @param filter
+     */
+    public sF(filter: (name: string) => boolean): this {
+        return this.setFilter(filter);
+    }
+
+    /**
+     * short for {@link resetFilter}.
+     */
+    public rsF() {
+        return this.resetFilter();
+    }
+
+    /**
+     * short for {@link addWhiteList}.
+     * @param names
+     */
+    public aW(...names: (string | string[])[]) {
+        return this.addWhiteList(...names);
+    }
+
+    /**
+     * short for {@link addBlackList}.
+     * @param names
+     */
+    public aB(...names: (string | string[])[]) {
+        return this.addBlackList(...names);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
+
+/**
+ * Log4Ts.
+ * pure TS 日志管理器.
+ * @desc 提供统一的日志管理.
+ * @desc 以及简单的过滤功能.
+ * @desc ---
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+ * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
+ * @version 1.0.0
+ */
+class Log4Ts {
+//#region Config
+    private _config: Log4TsConfig = new Log4TsConfig();
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    public log(announcer: Announcer, ...messages: (logString | string)[]): void;
+
+    public log(announcer: Announcer, ...messages: unknown[]): void;
+
+    /**
+     * debug log.
+     * @param announcer announcer with name.
+     * @param messages text.
+     */
+    public log(announcer: Announcer, ...messages: (logString | string | unknown)[]): void {
+        if (this._config.debugLevel !== DebugLevels.Dev || !this._config.checkAnnouncer(announcer)) return;
+
+        let title = true;
+        for (const msg of messages) {
+            let msgStr: string;
+            if (typeof msg === "string") {
+                msgStr = msg;
+            } else if (typeof msg === "function") {
+                msgStr = msg();
+            } else {
+                msgStr = msg.toString();
+            }
+
+            console.log(`${title ? announcer.name + ": " : `    `}${msgStr}`);
+            title = false;
+        }
+    }
+
+    public warn(announcer: Announcer, ...messages: (logString | string)[]): void;
+
+    public warn(announcer: Announcer, ...messages: unknown[]): void;
+
+    /**
+     * debug warn.
+     * @param announcer announcer with name.
+     * @param messages text.
+     */
+    public warn(announcer: Announcer, ...messages: (logString | string | unknown)[]): void {
+        if (this._config.debugLevel === DebugLevels.Silent || !this._config.checkAnnouncer(announcer)) return;
+
+        let title = true;
+        for (const msg of messages) {
+            let msgStr: string;
+            if (typeof msg === "string") {
+                msgStr = msg;
+            } else if (typeof msg === "function") {
+                msgStr = msg();
+            } else {
+                msgStr = msg.toString();
+            }
+
+            console.warn(`${title ? announcer.name + ": " : `    `}${msgStr}`);
+            title = false;
+        }
+    }
+
+    public error(announcer: Announcer, ...messages: (logString | string)[]): void;
+
+    public error(announcer: Announcer, ...messages: unknown[]): void;
+
+    /**
+     * debug error.
+     * @param announcer announcer with name.
+     * @param messages text.
+     */
+    public error(announcer: Announcer, ...messages: (logString | string | unknown)[]): void {
+        if (this._config.debugLevel === DebugLevels.Silent || !this._config.checkAnnouncer(announcer)) return;
+
+        let title = true;
+        for (const msg of messages) {
+            let msgStr: string;
+            if (typeof msg === "string") {
+                msgStr = msg;
+            } else if (typeof msg === "function") {
+                msgStr = msg();
+            } else {
+                msgStr = msg.toString();
+            }
+
+            console.error(`${title ? announcer.name + ": " : `    `}${msgStr}`);
+            title = false;
+        }
+    }
+
+    /**
+     * 设置配置.
+     * @param config
+     */
+    public setConfig(config: Log4TsConfig = undefined): this {
+        this._config = config ?? new Log4TsConfig();
+        return this;
+    }
+}
+
+export default new Log4Ts().setConfig();
---
+++ JavaScripts/depends/log4ts/README.md
@@ -0,0 +1,53 @@
+# Log4Ts
+
+Log4Ts 日志管理 in pure typescript。提供统一的日志管理以及简单的过滤功能。
+
+v1.0.0b  
+by LviatYi
+
+阅读该文档时，推荐安装以下字体：
+
+- [JetBrainsMono Nerd Font Mono][JetbrainsMonoNerdFont]
+- [Sarasa Mono SC][SarasaMonoSC]
+
+若出现乱码，其为 Nerd Font 的特殊字符，不影响段落语义。
+
+## Functional ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+它旨在提供如下便利：
+
+- [x] **因时制宜** 使用 DebugLevels 对日志进行分级控制。
+- [x] **恰如其分** 提供过滤功能，与 gitignore 配合实现客制化的日志选项。
+- [x] **主次分明** 对多行日志进行标题与缩进控制，以提升可读性。
+
+## Example ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+支持 **自定义配置** 以对你的环境进行私有的日志输出配置。以下是操作步骤：
+
+1. 在该文件夹下建立 Log4TsSelfConfig.ts 文件
+
+   如 `/project/JavaScripts/depends/log4ts/Log4TsSelfConfig.ts`
+
+2. 在该文件中编写配置
+
+    ```typescript
+    // recommend to add this file in gitignore
+    
+    import Log4Ts, { Log4TsConfig } from "./Log4Ts";
+    
+    Log4Ts.setConfig(
+        new Log4TsConfig()
+            .addBlackList("IDontWantThis"),
+    );
+    ```
+
+3. 将文件在 gitignore 中忽略
+
+    ```gitignore
+    # Custom
+    Log4TsSelfConfig.ts
+    ```
+
+[JetbrainsMonoNerdFont]: https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip@fallbackFont
+
+[SarasaMonoSC]: https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
