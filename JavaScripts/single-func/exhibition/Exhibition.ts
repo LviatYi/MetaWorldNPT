d2b9432722b3c86f903663c4e19e069c6c5bd78c
lviatyi <jiajun.yi@appshahe.com>
2023-12-15 19:15:33 +0800

    feat: update Exhibition1.4.1

--- JavaScripts/single-func/exhibition/Exhibition.ts
+++ JavaScripts/single-func/exhibition/Exhibition.ts
@@ -1,10 +1,10 @@
-import Camera = mw.Camera;
 import InputUtil = mw.InputUtil;
 import Vector2 = mw.Vector2;
 import Rotation = mw.Rotation;
 import StaleButton = mw.StaleButton;
 import InputBox = mw.InputBox;
 import InputTextLimit = mw.InputTextLimit;
+import EventListener = mw.EventListener;
 
 /**
  * 展台.
@@ -18,12 +18,13 @@ import InputTextLimit = mw.InputTextLimit;
  * @author LviatYi
  * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
  * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
- * @version 1.2.1
+ * @version 1.4.1
  */
 @Component
 export default class Exhibition extends mw.Script {
     //#region Member
-    private _eventListeners: EventListener[] = [];
+
+    private _eventListeners: EventListener[];
 
     /**
      * Prefab guid.
@@ -31,7 +32,6 @@ export default class Exhibition extends mw.Script {
     @mw.Property({
         displayName: "prefab guid",
         group: "综合配置 | 物体",
-        tooltip: "务必是客户端单端的. Prefab GameObject Tag 仅一种生效",
     })
     public itemPrefabGuid: string = "";
 
@@ -62,12 +62,6 @@ export default class Exhibition extends mw.Script {
     public itemTag: string = "ExhibitionItem";
 
     /**
-     * prefab location.
-     */
-    @mw.Property({displayName: "camera location offset", group: "综合配置 | 镜头", tooltip: "镜头相对 item 偏移量"})
-    public cameraLocationOffset: Vector = new Vector(-500, 0, 100);
-
-    /**
      * 最大自动旋转速度.
      */
     @mw.Property({displayName: "max auto rotation speed", group: "展台配置 | 旋转", tooltip: "最大自动旋转速度 °/s"})
@@ -91,7 +85,7 @@ export default class Exhibition extends mw.Script {
     @mw.Property({
         displayName: "destination",
         group: "展台配置 | 旋转",
-        tooltip: "旋转目标. Local Rotate Z. [-180,180)",
+        tooltip: "旋转目标. World Rotate Z. [-180,180)",
     })
     public destination: number = 0;
 
@@ -103,19 +97,19 @@ export default class Exhibition extends mw.Script {
         group: "展台配置 | 旋转",
         tooltip: "是否启用 旋转目标",
     })
-    public useDestination: boolean = true;
+    public useDestination: boolean = false;
 
     /**
      * 是否 以自动旋转开始.
      */
     @mw.Property({displayName: "isAutoRotateBegin", group: "展台配置 | 旋转", tooltip: "是否 以自动旋转开始"})
-    public isAutoRotate: boolean = false;
+    public isAutoRotate: boolean = true;
 
     /**
      * 是否 可手动的.
      */
     @mw.Property({displayName: "manualAble", group: "展台配置 | 手动", tooltip: "是否 可手动的"})
-    public manualAble: boolean = true;
+    public manualAble: boolean = false;
 
     /**
      * 手动旋转速度.
@@ -131,17 +125,15 @@ export default class Exhibition extends mw.Script {
 
     private _currentVelocity: number = 0;
 
-    private get isWatching() {
-        return Camera.currentCamera === this._camera;
-    }
-
     private _obj: GameObject = null;
-
-    private _camera: Camera = null;
 
     private _lastTouchPosition: Vector2 = Vector2.zero;
 
     private _touched: boolean = false;
+
+    private _validRunning: boolean = false;
+
+    private _lastCylinderNum: number = 0;
 
     private _lastEndTouchTime: number = 0;
 
@@ -160,6 +152,7 @@ export default class Exhibition extends mw.Script {
         }
 
         this.useUpdate = true;
+
         //#region Member init
 
         this._controllerCanvas = Canvas.newObject();
@@ -172,6 +165,8 @@ export default class Exhibition extends mw.Script {
 
         this._textFrom.inputTextLimit = InputTextLimit.LimitToInt;
         this._textTo.inputTextLimit = InputTextLimit.LimitToInt;
+        this._textFrom.hintString = "from";
+        this._textTo.hintString = "to";
         this._textFrom.size = new Vector2(200, 60);
         this._textFrom.position = new Vector2(0, 20);
         this._textTo.size = new Vector2(200, 60);
@@ -193,25 +188,11 @@ export default class Exhibition extends mw.Script {
         }
 
         console.log("M 键 切换 自动旋转.");
-        console.log("N 键 切换 使用旋转目标.");
-        console.log("H 键 切换 控制台显示");
-        console.log("P 键 Rotate!");
 
         InputUtil.onKeyDown(Keys.M, () => {
             this.isAutoRotate = !this.isAutoRotate;
+            this.useDestination = false;
             console.log(`切换 自动旋转: ${this.isAutoRotate}.`);
-        });
-        InputUtil.onKeyDown(Keys.N, () => {
-            this.useDestination = !this.useDestination;
-            console.log(`切换 使用旋转目标: ${this.useDestination}.`);
-        });
-        InputUtil.onKeyDown(Keys.H, () => {
-            this._controllerCanvas.renderOpacity = this._controllerCanvas.renderOpacity === 0 ? 1 : 0;
-            console.log(`切换 控制台显示: ${this._controllerCanvas.renderOpacity === 0}.`);
-        });
-        InputUtil.onKeyDown(Keys.P, () => {
-            this.onBtnClicked();
-            console.log(`Rotate!.`);
         });
 
         if (this.itemPrefabGuid && this.itemPrefabGuid !== "") {
@@ -228,7 +209,6 @@ export default class Exhibition extends mw.Script {
                 value => {
                     if (value) {
                         this._obj = value;
-                        this.attachCamera();
                     } else {
                         console.error("物体生成失败.\nprefab guid 指向一个无效的 prefab.");
                     }
@@ -238,16 +218,12 @@ export default class Exhibition extends mw.Script {
             this._obj = GameObject.findGameObjectById(this.itemGameObjectGuid);
             if (!this._obj) {
                 console.warn(`场景内不存在任何 guid 为 ${this.itemGameObjectGuid} 的物体.`);
-            } else {
-                this.attachCamera();
             }
         } else {
             this._obj = GameObject.findGameObjectsByTag(this.itemTag)[0];
             if (!this._obj) {
                 console.warn("场景内不存在任何带有 ExhibitionItem 标签的物品.");
                 console.warn("且配置了一个空的 prefab guid 与 game object guid.");
-            } else {
-                this.attachCamera();
             }
         }
         //#endregion ------------------------------------------------------------------------------------------
@@ -288,14 +264,23 @@ export default class Exhibition extends mw.Script {
             this._currentVelocity = 0;
         }
 
-        const currRotation = item.localTransform.rotation;
-        if (this.useDestination && Math.abs(currRotation.z - this.destination) < this.itemMaxAutoRotateSpeed * 1.1) {
-            item.worldTransform.rotation = new Rotation(
-                currRotation.x,
-                currRotation.y,
-                this.destination);
-            return;
+        const currRotation = item.worldTransform.rotation;
+
+        if (this.useDestination && this.willArrive(currRotation.z)) {
+            if (this._lastCylinderNum <= 0) {
+                if (this._validRunning) {
+                    item.worldTransform.rotation = new Rotation(
+                        currRotation.x,
+                        currRotation.y,
+                        this.destination);
+                    this.isAutoRotate = false;
+                    return;
+                }
+            } else {
+                --this._lastCylinderNum;
+            }
         }
+        this._validRunning = this._currentVelocity > 0;
         item.worldTransform.rotation = new Rotation(
             currRotation.x,
             currRotation.y,
@@ -306,28 +291,15 @@ export default class Exhibition extends mw.Script {
         return !this._touched && Date.now() - this._lastEndTouchTime > this.returnAutoDuration * 1e3;
     }
 
-    private attachCamera() {
-        if (!this._obj) return;
-        GameObject.asyncSpawn("Camera",
-            {
-                replicates: false,
-                transform: new Transform(
-                    this._obj
-                        .worldTransform
-                        .position
-                        .clone()
-                        .add(this.cameraLocationOffset),
-                    new Rotation(0, 0, 0),
-                    Vector.one),
-            },
-        ).then(
-            (value) => {
-                const camera = value as Camera;
-                camera.lock(this._obj);
-                this._camera = camera;
-                Camera.switch(value as Camera);
-            },
-        );
+    private clampValidDestinationAngle(origin: number) {
+        return ((origin + 180) % 360 | 0) - 180;
+    }
+
+    private willArrive(currZ: number): boolean {
+        const dist = this.isClockWise ?
+            this.destination + (this.destination < currZ ? 360 : 0) - currZ :
+            currZ - this.destination + (this.destination > currZ ? 360 : 0);
+        return this._currentVelocity > dist;
     }
 
     //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
@@ -338,31 +310,25 @@ export default class Exhibition extends mw.Script {
     //#region Event Callback
 
     private touchBegin = (index: number, location: Vector2) => {
-        if (!this.isWatching) return;
-
         this._touched = true;
         this._currentVelocity = 0;
         this._lastTouchPosition.set(location.x, location.y);
     };
 
     private touchEnd = (index: number, location: Vector2) => {
-        if (!this.isWatching) return;
-
         this._touched = false;
         this._lastEndTouchTime = Date.now();
     };
 
     private touchMove = (index: number, location: Vector2) => {
-        if (!this.isWatching) return;
-
         if (!this._obj) return;
         if (this._lastTouchPosition.equals(location, 1e-3)) return;
 
         let flag = this._lastTouchPosition.x - location.x > 0;
         const delta = this._lastTouchPosition.subtract(location).length;
-        let rotation = this._obj.localTransform.rotation;
+        let rotation = this._obj.worldTransform.rotation;
         rotation.z += (flag ? delta : -delta) * this.manualRotateSpeed;
-        this._obj.localTransform.rotation = rotation;
+        this._obj.worldTransform.rotation = rotation;
         this._lastTouchPosition.set(location.x, location.y);
     };
 
@@ -373,11 +339,16 @@ export default class Exhibition extends mw.Script {
         console.log("Rotate!");
         console.log(`from: ${from}, to: ${to}`);
 
-        const currentRotation = this._obj.localTransform.rotation;
-        this._obj.localTransform.rotation = new mw.Rotation(currentRotation.x, currentRotation.y, from);
-        this.destination = to;
+        this.isClockWise = to > from;
+
+        this._lastCylinderNum = Math.abs(to - from) / 360 | 0;
+
+        const currentRotation = this._obj.worldTransform.rotation;
+        this._obj.worldTransform.rotation = new Rotation(currentRotation.x, currentRotation.y, from);
+        this.destination = this.clampValidDestinationAngle(to);
         this.useDestination = true;
         this.isAutoRotate = true;
+        this._validRunning = false;
     };
     //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 }
