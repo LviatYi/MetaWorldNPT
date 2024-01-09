a3ecc2f248c3c10980af164f4e2995d7662f9a33
lviatyi <jiajun.yi@appshahe.com>
2024-01-09 19:22:15 +0800

    feat: update TestPanel

--- JavaScripts/test/TestPanel.ts
+++ JavaScripts/test/TestPanel.ts
@@ -3,6 +3,7 @@ import GToolkit from "../util/GToolkit";
 import Nolan from "../depend/nolan/Nolan";
 import Log4Ts from "../depend/log4ts/Log4Ts";
 import { TestModuleC } from "../module/TestModule";
+import ByteArray from "../depend/byteArray/ByteArray";
 import Player = mw.Player;
 import Camera = mw.Camera;
 
@@ -30,7 +31,7 @@ export class TestPanel extends TestPanel_Generate {
 //#endregion ------------------------------------------------------------------------------------------
 
 //#region Widget bind
-        this._nolan = new Nolan();
+        this._nolan = Nolan.getInstance();
         this.testButton.onClicked.add(this.onTestBtn0Click);
         this.testButton1.onClicked.add(this.onTestBtn1Click);
         this.testButton2.onClicked.add(this.onTestBtn2Click);
@@ -84,6 +85,63 @@ export class TestPanel extends TestPanel_Generate {
 
     private onTestBtn3Click = () => {
         Log4Ts.log(TestPanel, `test L click`);
+
+        testByteArray();
     };
 //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
+
+function testByteArray() {
+    const byteLength = 17;
+    const count = 10000;
+    const data = new Array(count);
+
+    for (let i = 0; i < count; ++i) {
+        data[i] = Math.random() * (2 ** byteLength) | 0;
+    }
+
+    Log4Ts.log(TestPanel, `generate data byteLength: ${byteLength},count: ${count}`);
+
+    const startTime = Date.now();
+    const byteArray = ByteArray.createInstance(count, byteLength);
+    for (let i = 0; i < count; ++i) {
+        byteArray.setValue(i, data[i]);
+    }
+
+    const createTime = Date.now();
+    const dataString = byteArray.toString();
+    const encodeTime = Date.now();
+    const byteArray2 = ByteArray.fromString(dataString, count, byteLength);
+    const decodeTime = Date.now();
+
+    Log4Ts.log(TestPanel, `byte array createTime: ${createTime - startTime}ms`);
+    Log4Ts.log(TestPanel, `string length: ${dataString.length}`);
+    Log4Ts.log(TestPanel, `pure string encodeTime: ${encodeTime - createTime}ms`);
+    Log4Ts.log(TestPanel, `pure string decodeTime: ${decodeTime - encodeTime}ms`);
+
+    for (let i = 0; i < count; ++i) {
+        if (data[i] !== byteArray2.getValue(i)) {
+            Log4Ts.error(testByteArray, `not work`);
+        }
+    }
+}
+
+function setValue(byteArray: ByteArray, x: number, y: number, value: boolean, rowCount: number) {
+    byteArray.setValue(x + y * rowCount, value);
+}
+
+function getValue(byteArray: ByteArray, x: number, y: number, value: boolean, rowCount: number): boolean {
+    return !!byteArray.getValue(x + y * rowCount);
+}
+
+function createByteArray(rowCount: number, columnCount: number): ByteArray {
+    return ByteArray.createInstance(rowCount * columnCount, 1);
+}
+
+function stringify(byteArray: ByteArray): string {
+    return byteArray.toString();
+}
+
+function parse(data: string, rowCount: number, columnCount: number, byteLength: number = 1): ByteArray {
+    return ByteArray.fromString(data, rowCount * columnCount, byteLength);
 }
