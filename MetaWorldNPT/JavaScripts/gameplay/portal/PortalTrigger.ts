d4e0037b6d7698cace57cfb9bab195d1cd1bb770
lviatyi <jiajun.yi@appshahe.com>
2024-04-03 11:49:49 +0800

    feat: add PortalTrigger

---
+++ MetaWorldNPT/JavaScripts/gameplay/portal/PortalTrigger.ts
@@ -0,0 +1,129 @@
+import Gtk from "../../util/GToolkit";
+import Nolan from "../../depend/nolan/Nolan";
+import Log4Ts from "../../depend/log4ts/Log4Ts";
+
+/**
+ * 传送门 Trigger.
+ *
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+ * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
+ */
+@Component
+export default class PortalTrigger extends mw.Script {
+    //#region Member
+
+    @Property({displayName: "传送目的地", group: "Config-location"})
+    public destination: mw.Vector = mw.Vector.zero;
+
+    @Property({displayName: "传送目标", group: "Config-location", tooltip: "传送目标的 guid. 优先生效."})
+    public destinationTargetGuid: string = "";
+
+    @Property({
+        displayName: "是否刷新玩家物体旋转",
+        tooltip: "true 时 将玩家物体旋转调整为 endRotation.",
+        group: "Config-rotation",
+    })
+    public isRefreshObjectRotation: boolean = true;
+
+    @Property({
+        displayName: "是否刷新相机旋转",
+        tooltip: "true 时 将玩家相机调整为 endRotation.",
+        group: "Config-rotation",
+    })
+    public isRefreshCameraRotation: boolean = true;
+
+    @Property({displayName: "角色目标旋转", group: "Config-rotation"})
+    public endRotation: Rotation = Rotation.zero;
+
+    @Property({
+        displayName: "是否保持原有速度",
+        tooltip: "若目的地过于贴近地面 原有速度将受摩擦力影响\n旋转将跟随 isRefreshObjectRotation",
+        group: "Config-speed",
+    })
+    public isHoldVelocity: boolean = false;
+
+    private _nolan: Nolan;
+
+    //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //region MetaWorld Event
+    protected onStart(): void {
+        super.onStart();
+
+        if (mw.SystemUtil.isServer()) {
+            return;
+        }
+        //region Member init
+        this._nolan = Nolan.getInstance();
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+        //region Widget bind
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+        //region Event subscribe
+        let gameObject: mw.GameObject = this.gameObject;
+        if (gameObject) {
+            let trigger = gameObject as mw.Trigger;
+            if (trigger) {
+                trigger.onEnter.add(this.onObjectEnter);
+            } else {
+                Log4Ts.error(PortalTrigger, `there is no trigger under this game object.`);
+            }
+        }
+        //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+    }
+
+    protected onDestroy(): void {
+        super.onDestroy();
+    }
+
+    //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //#region Method
+    //#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    //region Event Callback
+    private onObjectEnter = (obj: GameObject): void => {
+        if (Gtk.isSelfCharacter(obj)) {
+            this.transferPlayer(obj as Character);
+        }
+    };
+
+    protected transferPlayer(character: Character) {
+        Log4Ts.log(PortalTrigger, `player enter. playerId: ${character?.player?.playerId ?? "null"}`);
+
+        if (character) {
+            const player = character.player;
+            if (player) {
+                const velocity = character.velocity;
+
+                character.addImpulse(velocity.clone().multiply(mw.Vector.negOne), true);
+                character.worldTransform = new Transform(
+                    Gtk.isNullOrEmpty(this.destinationTargetGuid) ?
+                        this.destination :
+                        GameObject.findGameObjectById(this.destinationTargetGuid)?.worldTransform.position ?? this.destination,
+                    this.isRefreshObjectRotation ? this.endRotation : character.worldTransform.rotation,
+                    character.worldTransform.scale,
+                );
+
+                if (this.isRefreshCameraRotation) this._nolan.lookToward(this.endRotation.rotateVector(mw.Vector.forward));
+
+                if (this.isHoldVelocity) {
+                    let v = this.isRefreshObjectRotation ?
+                        this.endRotation.rotateVector(mw.Vector.forward).multiply(velocity.length) :
+                        velocity;
+
+                    character.addImpulse(v, true);
+                }
+            }
+        }
+    }
+
+    //endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
