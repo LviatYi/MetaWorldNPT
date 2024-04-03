f2edee9f8d01eb7f2223c012ba1ee0f7936d812d
lviatyi <jiajun.yi@appshahe.com>
2024-04-03 11:50:04 +0800

    feat: add TweenBench

--- MetaWorldNPT/JavaScripts/GameStart.ts
+++ MetaWorldNPT/JavaScripts/GameStart.ts
@@ -1,25 +1,11 @@
-import FloatPanel from "./lab/ui/float/FloatPanel";
 import TestModuleData, {TestModuleC, TestModuleS} from "./module/TestModule";
 import AuthModuleData, {AuthModuleC, AuthModuleS} from "./module/AuthModule";
-import BoardPanel from "./lab/ui/BoardPanel";
 import * as mwaction from "mwaction";
-import TweenElementPanelOld from "./lab/ui/tween/TweenElementPanelOld";
 import {VectorExt} from "./declaration/vectorext";
-import Log4Ts from "./depend/log4ts/Log4Ts";
-import TweenElementPanel from "./lab/ui/tween/TweenElementPanel";
-import Waterween from "./depend/waterween/Waterween";
 import SystemUtil = mw.SystemUtil;
 
 @Component
 export default class GameStart extends mw.Script {
-    private _floatPanel: FloatPanel;
-
-    private _startTime: number = Date.now();
-
-    private _useWaterween: boolean = false;
-
-    private _avg: number = 0;
-    private _sampleCount: number = 0;
 
 //region MetaWorld Event
     protected onStart(): void {
@@ -29,23 +15,6 @@ export default class GameStart extends mw.Script {
         mwaction;
         VectorExt.initialize();
 //region Member init
-//         UIService.show(TestPanel);
-        if (SystemUtil.isClient()) {
-            const now = Date.now();
-            const testCount = 2400;
-            // const testCount = 5;
-            for (let i = 0; i < testCount; ++i) {
-                if (this._useWaterween) {
-                    UIService.getUI(BoardPanel).addToMain(UIService.create(TweenElementPanel).uiObject);
-                } else {
-                    UIService.getUI(BoardPanel).addToMain(UIService.create(TweenElementPanelOld).uiObject);
-                }
-            }
-            Log4Ts.log(GameStart, `create ${testCount} task done.`,
-                `now use ${this._useWaterween ? "Waterween" : "Actions"}`,
-                `cost time: ${Date.now() - now}`,
-            );
-        }
 
         ModuleService.registerModule(AuthModuleS, AuthModuleC, AuthModuleData);
         ModuleService.registerModule(TestModuleS, TestModuleC, TestModuleData);
@@ -64,22 +33,6 @@ export default class GameStart extends mw.Script {
 
     protected onUpdate(dt: number): void {
         super.onUpdate(dt);
-        let startTime = Date.now();
-        if (startTime < this._startTime + 2e3) return;
-        if (this._useWaterween) {
-            Waterween.update();
-        } else {
-            actions.AcitonMgr.update(dt * 1000);
-        }
-        const cost = Date.now() - startTime;
-        if (this._sampleCount > 10) {
-            this._avg = (this._avg * (this._sampleCount - 10) + cost) / (this._sampleCount - 9);
-        }
-        ++this._sampleCount;
-        Log4Ts.log(GameStart, `all actions task done.`,
-            `now use ${this._useWaterween ? "Waterween" : "Actions"}`,
-            `cost time avg: ${this._avg}`,
-            `sample count: ${this._sampleCount}`);
     }
 
     protected onDestroy(): void {
