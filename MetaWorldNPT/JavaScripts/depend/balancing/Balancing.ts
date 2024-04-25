0bd1c26090a22db2cd0a86477e4268f0853a5207
lviatyi <jiajun.yi@appshahe.com>
2024-04-25 19:02:39 +0800

    feat: add Balancing

---
+++ MetaWorldNPT/JavaScripts/depend/balancing/Balancing.ts
@@ -0,0 +1,259 @@
+import {BulletTask, DoneStatus} from "./BulletTask";
+import Gtk, {Delegate, GtkTypes, Singleton} from "../../util/GToolkit";
+import Log4Ts from "../log4ts/Log4Ts";
+import SimpleDelegate = Delegate.SimpleDelegate;
+
+/**
+ * Balancing 负载均衡管理.
+ *
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+ * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
+ * @version 1.0.0
+ */
+export default class Balancing extends Singleton<Balancing>() {
+//#region Constant
+    public static readonly ASYNC_TASK_TIMEOUT_INFER = 10 * GtkTypes.Interval.PerSec;
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    /**
+     * 弹药箱.
+     * @type {Map<string, BulletTask[]>} taskTag -> task
+     * @private
+     */
+    private _bulletMap: Map<string, BulletTask[]>;
+
+    /**
+     * 弹匣.
+     * @type {Set<BulletTask>}
+     * @private
+     */
+    private _magazine: Set<BulletTask>;
+
+    /**
+     * 枪膛.
+     * @type {BulletTask[]}
+     * @private
+     */
+    private _bore: BulletTask[];
+
+    /**
+     * 已完成任务计数.
+     * @type {number}
+     * @private
+     */
+    private _doneCounter: number = 0;
+
+    /**
+     * 负载均衡阈值. ms
+     * @desc 当前计时大于阈值时触发负载均衡.
+     * @desc 剩余任务将等待下一次调用.
+     * @type {number}
+     */
+    public threshold: number = GtkTypes.Interval.Hz60;
+
+    /**
+     * 异步任务最大容量.
+     * @type {number}
+     */
+    public asyncBulletMaxCount: number = 10;
+
+    private asyncCounter: number = 0;
+
+    private _useDebug: boolean = false;
+
+//#region Event
+    /**
+     * 当 󰈸开火 时.
+     * @type {Delegate.SimpleDelegate<void>}.
+     */
+    public onFire: SimpleDelegate<void> = new Delegate.SimpleDelegate();
+
+    /**
+     * 当 󰄲空仓挂机 时.
+     * @desc 开火后所有子弹发射完毕.
+     * @type {Delegate.SimpleDelegate<void>}
+     */
+    public onHangUp: SimpleDelegate<void> = new Delegate.SimpleDelegate();
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Controller
+    /**
+     * 填弹 一个同步任务.
+     * @desc 制作一颗子弹
+     * @desc 当带有 tag 时 存放在弹药箱.
+     * @desc 否则直接压入弹匣 下次 fire 调用后将执行.
+     * @param {() => void} task
+     * @param {string} tag 任务标签.
+     *      - 当子弹任务没有标签时 被认为是一个单次任务.
+     * @param {() => void} onError
+     * @param {number} timeout
+     * @param {() => void} onTimeout
+     */
+    public press(task: () => void,
+                 tag: string = undefined,
+                 onError: () => void = undefined,
+                 timeout: number = undefined,
+                 onTimeout: () => void = undefined) {
+        const bullet = new BulletTask(tag)
+            .setTask(task)
+            .setOnError(onError)
+            .setTimeoutPeriod(timeout ?? (8 * GtkTypes.Interval.PerSec))
+            .setOnTimeout(onTimeout);
+
+        this.sortOut(tag, bullet);
+    }
+
+    /**
+     * 填弹 一个异步任务.
+     * @desc 制作一颗子弹
+     * @desc 当带有 tag 时 存放在弹药箱.
+     * @desc 否则直接压入弹匣 下次 fire 调用后将执行.
+     * @param {() => Promise<void>} task
+     * @param {string} tag 任务标签.
+     *      - 当子弹任务没有标签时 被认为是一个单次任务.
+     * @param {() => void} onError
+     * @param {number} timeout=Balancing.ASYNC_TASK_TIMEOUT_INFER 异步任务需要超时时间.
+     *      - 不能超过 {@link Balancing.ASYNC_TASK_TIMEOUT_INFER}.
+     * @param {() => void} onTimeout
+     */
+    public pressAsync(task: () => Promise<void>,
+                      tag: string = undefined,
+                      onError: () => void = undefined,
+                      timeout: number = undefined,
+                      onTimeout: () => void = undefined) {
+        const bullet = new BulletTask(tag)
+            .setAsyncTask(task)
+            .setOnError(onError)
+            .setTimeoutPeriod(
+                timeout === undefined || timeout === 0 ?
+                    Balancing.ASYNC_TASK_TIMEOUT_INFER :
+                    Math.min(Balancing.ASYNC_TASK_TIMEOUT_INFER, timeout))
+            .setOnTimeout(onTimeout);
+
+        this.sortOut(tag, bullet);
+    }
+
+    /**
+     * 装填 一组任务.
+     * @desc 将弹药箱中被指定 tag 引用的子弹装入弹匣.
+     * @param {string} tag
+     */
+    public load(tag: string) {
+        const bullets = this._bulletMap.get(tag);
+        if (Gtk.isNullOrEmpty(bullets)) {
+            return;
+        }
+
+        bullets.forEach(bullet => this._magazine.add(bullet));
+    }
+
+    /**
+     * 󰈸开火.
+     * @desc 将负载均衡处理任务执行.
+     * @desc 直到弹匣中没有子弹.
+     */
+    public fire() {
+        this._bore = Array.from(this._magazine);
+        this._magazine.clear();
+    }
+
+    public progress() {
+        return this._doneCounter / (this._doneCounter + this._bore.length);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Config
+
+    /**
+     * 设置 Update 函数.
+     * @desc 用于定时调用负载均衡处理器.
+     * @param {(callback: () => void) => unknown} updater
+     * @return {this}
+     */
+    public registerUpdater(updater: (callback: () => void) => unknown): this {
+        updater?.(this.trigger);
+        return this;
+    }
+
+    /**
+     * 设置负载均衡阈值. ms
+     * 默认为 60Hz.
+     * @param {number} duration
+     * @return {this}
+     */
+    public setThreshold(duration: number): this {
+        this.threshold = duration;
+        return this;
+    }
+
+    /**
+     * 开启调试.
+     * @param {boolean} enable
+     * @return {this}
+     */
+    public useDebug(enable: boolean = true): this {
+        this._useDebug = enable;
+        return this;
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+    private trigger = () => {
+        if (this._bore.length === 0) return;
+
+        let costTime = 0;
+        let atLeastDid = false;
+        let size = this._bore.length;
+
+        for (let i = 0; i < size; ++i) {
+            const bullet = this._bore.shift();
+            if (bullet.isAsync) {
+                if (this.asyncCounter < this.asyncBulletMaxCount) {
+                    ++this.asyncCounter;
+                    bullet.onDone.add((status) => {
+                        --this.asyncCounter;
+                        if (this._useDebug) Log4Ts.log(Balancing, `[${DoneStatus[status]}] async task done.`);
+                    });
+                    bullet.run();
+                    bullet.onDone.clear();
+                }
+            } else {
+                if (atLeastDid && (costTime + bullet?.avgCost ?? 0) > this.threshold) {
+                    continue;
+                }
+                atLeastDid = true;
+                bullet.onDone.add((status) => {
+                    costTime += bullet.getTakeTime();
+                    if (this._useDebug) Log4Ts.log(Balancing, `[${DoneStatus[status]}] async task done.`);
+                });
+                bullet.run();
+                bullet.onDone.clear();
+            }
+        }
+    };
+
+    /**
+     * 区分臭弹.
+     * @private
+     */
+    private sortOut(tag: string, bullet: BulletTask) {
+        if (Gtk.isNullOrEmpty(tag)) {
+            this._magazine.add(bullet);
+        } else {
+            let magazine = this._bulletMap.get(tag);
+            if (Gtk.isNullOrUndefined(magazine)) {
+                magazine = [];
+                this._bulletMap.set(tag, magazine);
+            }
+
+            magazine.push(bullet);
+        }
+    }
+}
---
+++ MetaWorldNPT/JavaScripts/depend/balancing/BulletTask.ts
@@ -0,0 +1,321 @@
+import Log4Ts from "../log4ts/Log4Ts";
+import {Delegate} from "../../util/GToolkit";
+import SimpleDelegate = Delegate.SimpleDelegate;
+
+const DEFAULT_BULLET_NAME = "";
+
+/**
+ * 任务状态.
+ */
+export enum TaskStatus {
+    /**
+     * 󰄱未处理.
+     */
+    Unhandled,
+    /**
+     * 󰐊运行中.
+     */
+    Running,
+    /**
+     * 󰄲完成.
+     */
+    Finished,
+}
+
+export enum DoneStatus {
+    /**
+     * 未知错误.
+     */
+    Null,
+    /**
+     * 完成.
+     */
+    Done,
+    /**
+     * 错误.
+     */
+    Error,
+    /**
+     * 超时未完成.
+     */
+    Timeout,
+}
+
+/**
+ * 子弹任务.
+ *
+ * ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟
+ * ⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄
+ * ⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄
+ * ⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄
+ * ⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ * @author LviatYi
+ * @font JetBrainsMono Nerd Font Mono https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+ * @fallbackFont Sarasa Mono SC https://github.com/be5invis/Sarasa-Gothic/releases/download/v0.41.6/sarasa-gothic-ttf-0.41.6.7z
+ */
+export class BulletTask {
+    public readonly tag: string;
+
+    private _startTime: number = undefined;
+
+    private _endTime: number = undefined;
+
+    private _avgCost: number = undefined;
+
+    private _avgSamplerCount: number = 0;
+
+    /**
+     * 是否 为异步子弹任务.
+     * @return {boolean}
+     */
+    public get isAsync() {
+        return !!this._asyncTask;
+    }
+
+    /**
+     * 任务平均用时.
+     * @return {number}
+     */
+    public get avgCost(): number {
+        return this._avgCost;
+    }
+
+    /**
+     * 获取当前状态.
+     * @return {TaskStatus}
+     */
+    public getCurrentState(): TaskStatus {
+        if (this._startTime === undefined) {
+            return TaskStatus.Unhandled;
+        } else if (this._endTime === undefined) {
+            return TaskStatus.Running;
+        } else {
+            return TaskStatus.Finished;
+        }
+    }
+
+    /**
+     * 获取任务已运行时间.
+     * @param {number} now
+     * @return {number | null}
+     */
+    public getTakeTime(now?: number): number | null {
+        if (now === undefined) now = Date.now();
+
+        switch (this.getCurrentState()) {
+            case TaskStatus.Unhandled:
+                return null;
+            case TaskStatus.Running:
+                return now - this._startTime;
+            case TaskStatus.Finished:
+                return this._endTime - this._startTime;
+            default:
+                this.logCurrentStateNotSupported();
+                return null;
+        }
+    }
+
+    private _task: () => void;
+
+    private _asyncTask: () => Promise<void>;
+
+    private _errorCallback: () => void;
+
+    private _timeoutPeriod: number;
+
+    private _timeoutCallback: () => void;
+
+    private _timeoutTimer: number;
+
+    constructor(tag?: string) {
+        this.tag = tag ?? DEFAULT_BULLET_NAME;
+    }
+
+//#region Event
+    public onDone: SimpleDelegate<DoneStatus> = new Delegate.SimpleDelegate();
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Config Builder
+
+    /**
+     * 设置任务.
+     * @param {() => void} task
+     * @return {this}
+     */
+    public setTask(task: () => void): this {
+        this._task = task;
+        this._asyncTask = undefined;
+        return this;
+    }
+
+    /**
+     * 清除任务.
+     * @return {this}
+     */
+    public clearTask(): this {
+        this._task = undefined;
+        return this;
+    }
+
+    /**
+     * 设置异步任务.
+     * @param {() => Promise<void>} task
+     * @return {this}
+     */
+    public setAsyncTask(task: () => Promise<void>): this {
+        this._asyncTask = task;
+        this._task = undefined;
+        return this;
+    }
+
+    /**
+     * 设置错误回调.
+     * @param {() => void} onError
+     * @return {this}
+     */
+    public setOnError(onError: () => void) {
+        this._errorCallback = onError;
+        return this;
+    }
+
+    /**
+     * 设置超时时间.
+     * @param {number} timeout
+     * @return {this}
+     */
+    public setTimeoutPeriod(timeout: number): this {
+        this._timeoutPeriod = timeout;
+        return this;
+    }
+
+    /**
+     * 设置超时回调.
+     * @param {() => void} callback
+     * @return {this}
+     */
+    public setOnTimeout(callback: () => void): this {
+        this._timeoutCallback = callback;
+        return this;
+    }
+
+    /**
+     * 运行.
+     */
+    public run(): void {
+        switch (this.getCurrentState()) {
+            case TaskStatus.Unhandled:
+            case TaskStatus.Finished:
+                this.tryRunTaskHandler();
+                break;
+            case TaskStatus.Running:
+                break;
+            default:
+                this.logCurrentStateNotSupported();
+                return null;
+        }
+    }
+
+    private tryRunTaskHandler() {
+        if (!this._task && !this._asyncTask) return;
+        const startTime = Date.now();
+        if (this._task) {
+            try {
+                this.resetTimer(startTime, false);
+                this._task();
+                if (this._timeoutPeriod !== undefined &&
+                    Date.now() - startTime > this._timeoutPeriod) {
+                    try {
+                        this._timeoutCallback?.();
+                    } catch (e) {
+                        this.logErrorInTimeoutCallback(e);
+                    }
+                }
+                this.doneTimer(DoneStatus.Done);
+            } catch (e) {
+                this.handlerError(e);
+            }
+        } else {
+            this.resetTimer(startTime, true);
+            this._asyncTask()
+                .then(
+                    () => () => this.doneTimer(undefined, DoneStatus.Done),
+                    e => {
+                        this.handlerError(e);
+                        this.doneTimer(undefined, DoneStatus.Error);
+                    }
+                )
+                .catch(e => {
+                    this.handlerError(e);
+                    this.doneTimer(undefined, DoneStatus.Error);
+                });
+        }
+    }
+
+    private resetTimer(now?: number, setTimeout: boolean = false) {
+        if (now === undefined) now = Date.now();
+        this._startTime = now;
+        this._endTime = undefined;
+        if (setTimeout) this.setTimeoutTimer();
+    }
+
+    private doneTimer(now?: number, doneStatus: DoneStatus = DoneStatus.Done) {
+        if (now === undefined) now = Date.now();
+        this._endTime = now;
+        this.recordTakeTime(now);
+        this.clearTimeoutTimer();
+        this.onDone.invoke(doneStatus);
+    }
+
+    private handlerError(e: unknown) {
+        this.logErrorInTask(e);
+        try {
+            this._errorCallback?.();
+        } catch (e) {
+            this.logErrorInErrorCallback(e);
+        }
+    }
+
+    private recordTakeTime(now: number) {
+        const t = this.getTakeTime(now);
+        this._avgCost = ((this._avgCost ?? 0) * this._avgSamplerCount + t) / (this._avgSamplerCount + 1);
+        ++this._avgSamplerCount;
+    }
+
+    private setTimeoutTimer() {
+        if (!this._timeoutPeriod) return undefined;
+        this._timeoutTimer = mw.setTimeout(() => {
+            try {
+                this._timeoutCallback?.();
+                this.doneTimer(undefined, DoneStatus.Timeout);
+            } catch (e) {
+                this.logErrorInTimeoutCallback(e);
+            }
+        }, this._timeoutPeriod);
+    }
+
+    private clearTimeoutTimer() {
+        if (!this._timeoutTimer) return;
+        mw.clearTimeout(this._timeoutTimer);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+
+//#region Logs
+    public logCurrentStateNotSupported() {
+        Log4Ts.error(BulletTask, `TaskState ${this.getCurrentState()} is not supported.`);
+    }
+
+    public logErrorInTask(e: unknown) {
+        Log4Ts.error(BulletTask, `error occurs in task.`, e);
+    }
+
+    public logErrorInErrorCallback(e: unknown) {
+        Log4Ts.error(BulletTask, `error occurs in error callback.`, e);
+    }
+
+    public logErrorInTimeoutCallback(e: unknown) {
+        Log4Ts.error(BulletTask, `error occurs in timeout callback.`, e);
+    }
+
+//#endregion ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠐⠒⠒⠒⠒⠚⠛⣿⡟⠄⠄⢠⠄⠄⠄⡄⠄⠄⣠⡶⠶⣶⠶⠶⠂⣠⣶⣶⠂⠄⣸⡿⠄⠄⢀⣿⠇⠄⣰⡿⣠⡾⠋⠄⣼⡟⠄⣠⡾⠋⣾⠏⠄⢰⣿⠁⠄⠄⣾⡏⠄⠠⠿⠿⠋⠠⠶⠶⠿⠶⠾⠋⠄⠽⠟⠄⠄⠄⠃⠄⠄⣼⣿⣤⡤⠤⠤⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+}
