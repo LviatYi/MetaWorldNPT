2366e79fd905bbe455e3f0f560f7cef3ac525851
lviatyi <jiajun.yi@appshahe.com>
2024-08-13 19:00:26 +0800

    feat: update AutoExportAllNodeDefineForMw

--- MetaWorldNPT/JavaScripts/depend/behavior-tree-4-ts-mw-export/AutoExportAllNodeDefineForMw.ts
+++ MetaWorldNPT/JavaScripts/depend/behavior-tree-4-ts-mw-export/AutoExportAllNodeDefineForMw.ts
@@ -4,23 +4,24 @@ import { collectAllNodeDef } from "../behavior-tree-4-ts";
 const EXPORT_BEHAVIOR_TREE_NODES_DEFINE_STORAGE_KEY =
     "__BEHAVIOR_TREE_NODES_DEFINE_EXPORTED__";
 
-function autoExportAllNodeDefineForMw() {
-    mw.TimeUtil.onEnterFrame.remove(autoExportAllNodeDefineForMw);
+function autoExportAllNodeDefineForMw(clipboard: boolean = false) {
+    mw.TimeUtil.onEnterFrame.remove(autoExportHandler);
     const res = collectAllNodeDef();
     Log4Ts.log({name: "BehaviorTreeManager"},
         `Output Nodes: `,
         `${JSON.stringify(res)}`,
         `saved in DataStorage by key ${EXPORT_BEHAVIOR_TREE_NODES_DEFINE_STORAGE_KEY}`,
-        `saved in ClipBoard.`);
+        `${clipboard ? "saved in ClipBoard." : ""}`)
     if (mw.SystemUtil.isServer()) {
         mw.DataStorage.asyncSetData(
             EXPORT_BEHAVIOR_TREE_NODES_DEFINE_STORAGE_KEY,
             res);
     }
     if (mw.SystemUtil.isClient()) {
-        mw.StringUtil.clipboardCopy(JSON.stringify(res));
+        clipboard && mw.StringUtil.clipboardCopy(JSON.stringify(res));
     }
-
 }
 
-mw.TimeUtil.onEnterFrame.add(autoExportAllNodeDefineForMw);
+const autoExportHandler = () => autoExportAllNodeDefineForMw(false);
+
+mw.TimeUtil.onEnterFrame.add(autoExportHandler);
